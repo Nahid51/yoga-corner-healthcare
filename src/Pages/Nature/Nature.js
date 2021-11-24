fcf14f0eca597e09a99bd51d93c71652d480abec
Nahid51 <hasannahidnahid@gmail.com>
2021-11-24 15:47:34 +0600

    responsive nature section

--- src/Pages/Nature/Nature.js
+++ src/Pages/Nature/Nature.js
@@ -10,8 +10,8 @@ const Nature = () => {
             <Card className="bg-dark text-white mb-5 border-0">
                 <Card.Img className="bg-img" src="https://i.ibb.co/Hrsw05b/slogan-bg.jpg" alt="Card image" />
                 <Card.ImgOverlay className="text-center">
-                    <Card.Title style={{ color: "#2ACB35" }} className="logo fs-2">A Natural way of improving your health.</Card.Title>
-                    <Card.Text style={{ fontFamily: "'Poppins', sans-serif", fontWeight: "300" }} className="mb-5">
+                    <Card.Title style={{ color: "#2ACB35" }} className="logo font1">A Natural way of improving your health.</Card.Title>
+                    <Card.Text style={{ fontFamily: "'Poppins', sans-serif", fontWeight: "300" }} className="mb-3 font2">
                         Yoga Corner look at exercise, eating, wellness and food not just as calories and weight <br /> loss, but also emotional, mental and physical.
                     </Card.Text>
                     <Card.Text>Feel a better life</Card.Text>
